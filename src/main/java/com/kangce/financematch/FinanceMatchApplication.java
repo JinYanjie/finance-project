6ca52a640ce189528bea5f22f9a65f173638daf2
RoamSky <1244797924@qq.com>
2019-02-26 13:29:16 +0800

    财务系统比赛 第一次提交

---
+++ src/main/java/com/kangce/financematch/FinanceMatchApplication.java
@@ -0,0 +1,13 @@
+package com.kangce.financematch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceMatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceMatchApplication.class, args);
+    }
+
+}
