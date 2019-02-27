c885e567dc46b571a512250241b2f9bdf81f28c4
jyj <1052048733@qq.com>
2019-02-27 13:36:53 +0800

    数据库链接测试

--- src/test/java/com/kangce/financematch/FinanceMatchApplicationTests.java
+++ src/test/java/com/kangce/financematch/FinanceMatchApplicationTests.java
@@ -2,15 +2,25 @@ package com.kangce.financematch;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FinanceMatchApplicationTests {
-
+    @Autowired
+    DataSource dataSource;
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws SQLException {
+        System.out.println("aaaa"+dataSource.getClass());
+        Connection connection = dataSource.getConnection();
+        System.out.println("aaaa"+connection);
+        connection.close();
     }
 
 }
