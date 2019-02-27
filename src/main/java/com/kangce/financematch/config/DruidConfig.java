dda0329814345202d0202f4e01592225e8cc1ddb
jyj <1052048733@qq.com>
2019-02-27 15:12:43 +0800

    配置好了druid

---
+++ src/main/java/com/kangce/financematch/config/DruidConfig.java
@@ -0,0 +1,50 @@
+package com.kangce.financematch.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 使用application.yml里面配置 Druid的特殊属性
+ */
+@Configuration
+public class DruidConfig {
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DataSource druid(){
+        return new DruidDataSource();
+    }
+
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        Map<String,String> initParames=new HashMap<>();
+        initParames.put("loginUsername","admin");
+        initParames.put("loginPassword","123456");
+        initParames.put("allow","");
+        initParames.put("deny","123456");
+        bean.setInitParameters(initParames);
+        return bean;
+    }
+//    监控网页可以通过此查看    http://localhost:8080/druid/sql.html
+    @Bean
+    public FilterRegistrationBean webStatFilter(){
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.setFilter(new WebStatFilter());
+        Map<String,String> initParames=new HashMap<>();
+        initParames.put("exclusions","*.js,*.css,/druid/*");
+        bean.setInitParameters(initParames);
+        bean.setUrlPatterns(Arrays.asList("/*"));
+        return bean;
+    }
+}
