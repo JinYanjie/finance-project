887247b640acbe2c8dc714eb96bda6a38c3c524d
jyj <1052048733@qq.com>
2019-03-06 14:58:25 +0800

    返回信息改动

--- finance-main/src/main/java/com/kangce/main/controller/DepartmentController.java
+++ finance-main/src/main/java/com/kangce/main/controller/DepartmentController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
 
 @Controller
@@ -18,56 +19,64 @@ public class DepartmentController {
     @Autowired
     DepartmentService departmentService;
 
-    @RequestMapping(value = "/add",method = RequestMethod.POST)
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
     @ResponseBody
-    public Object add(@RequestParam("name") String name ){
-        int count = departmentService.addDepartment(name);
+    public Object add(@RequestParam("name") String name) {
+
+        int count = 0;
+        try {
+            count = departmentService.addDepartment(name);
+        } catch (Exception e) {
+            if (e instanceof org.springframework.dao.DuplicateKeyException) {
+                return new CommonResult().failed(name + "已存在");
+            }
+        }
         if (count > 0) {
             return new CommonResult().success(count);
         }
-        return new CommonResult().failed();
+        return new CommonResult().failed("");
     }
 
     @RequestMapping("/update")
     @ResponseBody
-    public Object updateDepartment(@RequestParam("id") byte id,@RequestParam("name") String name){
+    public Object updateDepartment(@RequestParam("id") byte id, @RequestParam("name") String name) {
         int count = departmentService.updateDepartment(id, name);
         if (count > 0) {
             return new CommonResult().success(count);
         }
-        return new CommonResult().failed();
+        return new CommonResult().failed("");
     }
 
     @RequestMapping("/delete")
     @ResponseBody
-    public Object deleteDepartment(@RequestParam("id") byte id){
+    public Object deleteDepartment(@RequestParam("id") byte id) {
         int count = departmentService.deleteDepartment(id);
         if (count > 0) {
             return new CommonResult().success(count);
         }
-        return new CommonResult().failed();
+        return new CommonResult().failed("");
     }
 
     @RequestMapping("/delete/batch")
     @ResponseBody
-    public Object deleteBatch(List<Byte> ids){
+    public Object deleteBatch(List<Byte> ids) {
         int count = departmentService.deletePartDepartment(ids);
         if (count > 0) {
             return new CommonResult().success(count);
         }
-        return new CommonResult().failed();
+        return new CommonResult().failed("");
     }
 
     @RequestMapping("/all")
     @ResponseBody
-    public Object listAll(){
+    public Object listAll() {
         List<Department> departments = departmentService.listAllDepartment();
         return new CommonResult().success(departments);
     }
 
     @RequestMapping("/item")
     @ResponseBody
-    public Object getDepartment(@RequestParam("id") byte id){
+    public Object getDepartment(@RequestParam("id") byte id) {
         List<Department> departments = departmentService.listDepartment(id);
 
 
