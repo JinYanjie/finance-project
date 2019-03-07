f281a747ce226019b91dc572ccdb6c8b1b650fa9
RoamSky <1244797924@qq.com>
2019-03-07 17:37:05 +0800

    添加注册接口

--- finance-main/src/main/java/com/kangce/main/controller/UserController.java
+++ finance-main/src/main/java/com/kangce/main/controller/UserController.java
@@ -6,20 +6,25 @@ import com.kangce.main.dto.CommonResult;
 import com.kangce.main.service.RedisService;
 import com.kangce.main.service.UserService;
 import com.kangce.main.util.JwtTokenUtil;
+import com.kangce.main.util.TextUtils;
 import com.kangce.mybatis.model.LoginSuccess;
 import com.kangce.mybatis.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     UserService userService;
@@ -54,7 +59,7 @@ public class UserController {
                 LoginSuccess loginSuccess = new LoginSuccess();
                 loginSuccess.setToken(token);
                 loginSuccess.setUser(targetUser);
-                commonResult.setData(loginSuccess);
+                commonResult.success(loginSuccess);
 
                 //存入redis
                 redisService.set(token+user.getPhone(),simpleDateFormat.format(new Date()));
@@ -64,6 +69,54 @@ public class UserController {
 
         return commonResult;
     }
+
+    @Transactional
+    @PostMapping("/register")
+    public Object register(@RequestParam(value = "userName",defaultValue = "") String userName,
+                           @RequestParam("phone") String phone,
+                           @RequestParam("password") String password,
+                           @RequestParam(value = "level",defaultValue = "1") int level,
+                           @RequestParam(value = "email",defaultValue = "") String email,
+                           @RequestParam(value = "company",defaultValue = "") String company
+                           ){
+
+        CommonResult commonResult = new CommonResult();
+
+        User user = new User();
+        if(TextUtils.isEmpty(userName)){
+            user.setUsername("用户_"+phone.substring(phone.length()-3,phone.length()));
+        }else{
+            user.setUsername(userName);
+        }
+        user.setPhone(phone);
+        user.setPassword(password);
+        user.setLevel(level);
+        user.setEmail(email);
+        user.setCompany(company);
+
+        int num = 0;
+        try {
+            num = userService.addUser(user);
+        }catch (Exception exception){
+            LOGGER.warn("添加用户失败: "+exception.getMessage());
+            return commonResult.failed("注册失败,该手机号已存在");
+        }
+
+        if (num==1){
+            User targetUser = userService.findUserByPhone(user.getPhone()).get(0);
+            String token = jwtTokenUtil.generateToken(targetUser);
+            LoginSuccess loginSuccess = new LoginSuccess();
+            loginSuccess.setToken(token);
+            loginSuccess.setUser(targetUser);
+            commonResult.success(loginSuccess);
+
+            //存入redis
+            redisService.set(token+user.getPhone(),simpleDateFormat.format(new Date()));
+        }
+
+        return commonResult;
+    }
+
 
 
     @UserLoginToken
